c7b638be963f397b5d7f302c866afcfe0d36cfc5
unknown <Linda@inner.local>
2020-11-03 16:45:56 +0800

    20201103，用mcc放了i2c的程式

---
+++ lcd.c
@@ -0,0 +1,13 @@
+/*
+ * File:   lcd.c
+ * Author: Linda
+ *
+ * Created on November 3, 2020, 3:41 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=I2C_MCC_20201103.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/I2C_MCC_20201103.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=i2cmcc20201103.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/i2cmcc20201103.x.tar
